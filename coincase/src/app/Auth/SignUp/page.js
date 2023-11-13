88360a74fc3c7fb4bdf223ca09350127fa5a8094
Stepan Muradyan <chewableanimal@gmail.com>
2023-11-13 02:15:05 -0800

    Added password requirements for sign up + styling

--- coincase/src/app/Auth/SignUp/page.js
+++ coincase/src/app/Auth/SignUp/page.js
@@ -16,54 +16,189 @@ import {
   List,
   ListIcon,
   ListItem,
+  HStack,
+  VStack,
+  Divider,
+  Center,
+  StackDivider,
 } from "@chakra-ui/react";
 
-import {InfoOutlineIcon} from "@chakra-ui/icons"
+import { InfoOutlineIcon, CheckIcon, NotAllowedIcon } from "@chakra-ui/icons";
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const [focusPassword, setFocusPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
+
   const handleFocus = () => {
     if (!focusPassword) {
       setFocusPassword(true);
     }
   };
-  const handleClick = () => setShowPassword(!showPassword);
+  const handleShow = () => setShowPassword(!showPassword);
+
+  const handleInput = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    if (e.target.name == 'password' && e.target.value !== "") {
+      setFocusPassword(true);
+    } else if (e.target.name == "password" && e.target.value === "") {
+      setFocusPassword(false);
+    }
+    console.log(formData.password);
+  };
+
+  const checkPasswordRequirements = (password) => {
+    const requirements = [
+      /.{8,}/, // Minimum length of 8 characters
+      /[A-Z]/, // Contains an uppercase letter
+      /[a-z]/, // Contains a lowercase letter
+      /[0-9]/, // Contains a number
+      /[!@#\$%\^\&*\)\(+=._-]/, // Contains a special character
+    ];
+
+    return requirements.map((regex) => regex.test(password));
+  };
+
+  const isValidPassword = (password) => {
+    const metRequirements = checkPasswordRequirements(password);
+    return metRequirements.every((req) => req);
+  };
+
   return (
     <div>
-      <Container w="500px" centerContent>
+      <Container w="750px" centerContent>
         <h1>Create An Account!</h1>
         <FormControl>
-          <FormLabel>First Name</FormLabel>
-          <Input variant="flushed" placeholder="First Name" />
-          <FormLabel>Last Name</FormLabel>
-          <Input variant="flushed" placeholder="Last Name" />
-          <FormLabel>Email</FormLabel>
-          <Input variant="flushed" placeholder="Email" />
-          <FormLabel>Password</FormLabel>
-          <InputGroup>
+          <VStack spacing={5}>
+            <HStack spacing={2} divider={<StackDivider />}>
+              <Input
+                variant="flushed"
+                placeholder="First Name"
+                name="firstName"
+                _placeholder={{ opacity: 0.8, color: "gray.500" }}
+                focusBorderColor="pink.400"
+              />
+              <Input
+                variant="flushed"
+                placeholder="Last Name"
+                name="lastName"
+                _placeholder={{ opacity: 0.8, color: "gray.500" }}
+                focusBorderColor="pink.400"
+              />
+            </HStack>
             <Input
               variant="flushed"
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              onFocus={handleFocus}
+              placeholder="Email"
+              name="email"
+              _placeholder={{ opacity: 0.8, color: "gray.500" }}
+              focusBorderColor="pink.400"
             />
-            <InputRightElement>
-              <Button h="1.75rem" size="sm" onClick={handleClick}>
-                {showPassword ? "Hide" : "Show"}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-          <Collapse in={focusPassword} animateOpacity>
-            <Box>
-              <List spacing={3}>
-                <ListItem>
-                  <ListIcon as={InfoOutlineIcon} color="gray.500" />
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                </ListItem>
+            <InputGroup>
+              <Input
+                variant="flushed"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                name="password"
+                onChange={handleInput}
+                _placeholder={{ opacity: 0.8, color: "gray.500" }}
+                focusBorderColor="pink.400"
+              />
+              <InputRightElement>
+                <Button h="1.75rem" size="sm" onClick={handleShow}>
+                  {showPassword ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <Collapse in={focusPassword} animateOpacity>
+              <Box>
+                <List spacing={1}>
+                  <ListItem>
+                    <ListIcon
+                      as={
+                        checkPasswordRequirements(formData.password)[0]
+                          ? CheckIcon
+                          : NotAllowedIcon
+                      }
+                      color={
+                        checkPasswordRequirements(formData.password)[0]
+                          ? "green.500"
+                          : "red.500"
+                      }
+                    />
+                    At least 8 characters
+                  </ListItem>
+                  <ListItem>
+                    <ListIcon
+                      as={
+                        checkPasswordRequirements(formData.password)[1]
+                          ? CheckIcon
+                          : NotAllowedIcon
+                      }
+                      color={
+                        checkPasswordRequirements(formData.password)[1]
+                          ? "green.500"
+                          : "red.500"
+                      }
+                    />
+                    At least 1 uppercase letter
+                  </ListItem>
+                  <ListItem>
+                    <ListIcon
+                      as={
+                        checkPasswordRequirements(formData.password)[2]
+                          ? CheckIcon
+                          : NotAllowedIcon
+                      }
+                      color={
+                        checkPasswordRequirements(formData.password)[2]
+                          ? "green.500"
+                          : "red.500"
+                      }
+                    />
+                    At least 1 lowercase character
+                  </ListItem>
+                  <ListItem>
+                    <ListIcon
+                      as={
+                        checkPasswordRequirements(formData.password)[3]
+                          ? CheckIcon
+                          : NotAllowedIcon
+                      }
+                      color={
+                        checkPasswordRequirements(formData.password)[3]
+                          ? "green.500"
+                          : "red.500"
+                      }
+                    />
+                    At least 1 number
+                  </ListItem>
+                  <ListItem>
+                    <ListIcon
+                      as={
+                        checkPasswordRequirements(formData.password)[4]
+                          ? CheckIcon
+                          : NotAllowedIcon
+                      }
+                      color={
+                        checkPasswordRequirements(formData.password)[4]
+                          ? "green.500"
+                          : "red.500"
+                      }
+                    />
+                    At least 1 special character
+                  </ListItem>
                 </List>
-            </Box>
-          </Collapse>
+              </Box>
+            </Collapse>
+            <Center>
+              <Button>Submit</Button>
+            </Center>
+          </VStack>
         </FormControl>
       </Container>
     </div>
