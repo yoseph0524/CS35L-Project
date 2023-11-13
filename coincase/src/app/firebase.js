54c11c050ee4aa2400ff7a3163ced2cd1faf2c95
Matthew-Tapia <matthewtapia8@gmail.com>
2023-11-12 21:50:58 -0800

    Added firestore + sent firebase config to environment variables

--- coincase/src/app/firebase.js
+++ coincase/src/app/firebase.js
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBwLRGK0FwWt3niyJ3Bh-lwUznuxazb70s",
-  authDomain: "coincase-b1337.firebaseapp.com",
-  projectId: "coincase-b1337",
-  storageBucket: "coincase-b1337.appspot.com",
-  messagingSenderId: "335607761170",
-  appId: "1:335607761170:web:e920e911e32b0edd9f0bcb",
-  measurementId: "G-ZQZRFYWXG2",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const db = getFirestore(app);
