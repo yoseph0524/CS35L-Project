08eee59cf6ba21d9d34fb125b6dd564c16ff33f6
Stepan Muradyan <chewableanimal@gmail.com>
2023-11-13 01:33:24 -0800

    Added chakra ui to provider and added form components

--- coincase/src/app/Auth/SignUp/page.js
+++ coincase/src/app/Auth/SignUp/page.js
@@ -1,17 +1,73 @@
-import React from 'react'
+"use client";
+
+import { React, useState } from "react";
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  InputRightElement,
+  Button,
+  InputGroup,
+  Container,
+  Collapse,
+  Box,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/react";
+
+import {InfoOutlineIcon} from "@chakra-ui/icons"
 
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [focusPassword, setFocusPassword] = useState(false);
+  const handleFocus = () => {
+    if (!focusPassword) {
+      setFocusPassword(true);
+    }
+  };
+  const handleClick = () => setShowPassword(!showPassword);
   return (
     <div>
-      <h1>Sign Up</h1>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <button>Submit</button>
-      </form>
+      <Container w="500px" centerContent>
+        <h1>Create An Account!</h1>
+        <FormControl>
+          <FormLabel>First Name</FormLabel>
+          <Input variant="flushed" placeholder="First Name" />
+          <FormLabel>Last Name</FormLabel>
+          <Input variant="flushed" placeholder="Last Name" />
+          <FormLabel>Email</FormLabel>
+          <Input variant="flushed" placeholder="Email" />
+          <FormLabel>Password</FormLabel>
+          <InputGroup>
+            <Input
+              variant="flushed"
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              onFocus={handleFocus}
+            />
+            <InputRightElement>
+              <Button h="1.75rem" size="sm" onClick={handleClick}>
+                {showPassword ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <Collapse in={focusPassword} animateOpacity>
+            <Box>
+              <List spacing={3}>
+                <ListItem>
+                  <ListIcon as={InfoOutlineIcon} color="gray.500" />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                </ListItem>
+                </List>
+            </Box>
+          </Collapse>
+        </FormControl>
+      </Container>
     </div>
   );
 }
 
-export default SignUp
+export default SignUp;
